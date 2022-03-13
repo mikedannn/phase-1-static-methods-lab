4c62cd2646f3e2eb75edb6b55e272c2bf6a17e69
Michael <michael.daniels1013@gmail.com>
2022-03-12 20:25:25 -0600

    commit tree a0d72f8d419f4999f71916497f6def819f0c23c1

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9-' '-]/gi, '');
+  }
+
+  static titleize(string) {
+    const noCapList = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let modifiedString = string.split(' ').map(s => {
+      if (!noCapList.includes(s)) {
+        return Formatter.capitalize(s)
+      } else {
+        return s;
+      }
+    }).join(" ")
+    return Formatter.capitalize(modifiedString);
+  }
 }
